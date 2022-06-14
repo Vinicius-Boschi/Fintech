071a70711189fb9868b4431a4054c163e491b451
Vinicius Boschi <vinicius.yg@gmail.com>
2022-06-14 19:20:51 -0300

    Adicionando projeto Fintech com tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#7280EE',
+        input: '#E8E0E3'
+      },
+    },
+  },
+  plugins: [],
+}
