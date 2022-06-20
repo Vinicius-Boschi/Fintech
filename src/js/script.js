a6e90cae9afd8d481e99260a523ccf1c2c78450b
Vinicius Boschi <vinicius.yg@gmail.com>
2022-06-20 17:46:11 -0300

    Adicionando validação de formulário

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,41 @@
-let button = document.querySelector("#mobile-button")
-let links = document.querySelector("#mobile-menu")
-let list = document.querySelector("#mobile-list")
+let input = document.querySelector("#password")
+let imgEye = document.querySelector("#img")
+let rptpassword = document.querySelector("#rptpassword")
+let otherImg = document.querySelector("#otherImg")
+let check = document.querySelector("#checkEqual")
 
-button.addEventListener("click", function() {
-    const divHeight = links.getBoundingClientRect().height
-    const listHeight = list.getBoundingClientRect().height
-    
-    console.log(divHeight)
-    console.log(listHeight)
-
-    if (divHeight === 0) {
-        links.style.height = `${listHeight}px`
-    } else {
-        links.style.height = 0
-        console.log('tchau')
-    }
+imgEye.addEventListener("click", function() {
+    input.type = input.type == 'text' ? 'password' : 'text' // Se a condição for verdadeira ele mostra a senha em texto, se não ele volta a esconder ela.
 })
+
+otherImg.addEventListener("click", function() {
+    rptpassword.type = rptpassword.type == 'text' ? 'password' : 'text'
+})
+
+function strongPassword() { 
+	let numbers = /([0-9])/
+	let tiny = /([a-z])/
+	let uppercase = /([A-Z])/
+	let diferent = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/
+
+	if ($('#password').val().length < 6) { 
+        // Se for menor que 6, mostra essa mensagem na tela.
+		$('#status').html("<p class='text-red-500 font-bold'>Fraco, insira no mínimo 6 letras</p>")
+	} else {  	
+		if ($('#password').val().match(numbers) && $('#password').val().match(tiny) && $('#password').val().match(uppercase) && $('#password').val().match(diferent)) { 
+            // Se tiver todos esses atributos, mostra essa.
+			$('#status').html("<p class='text-green-500 font-bold'>Forte</p>")
+		} else { 
+            // Se só tiver alguns, mostra essa.
+			$('#status').html("<p class='text-orange-500 font-bold'>Médio</p>")
+		}
+	}
+}
+
+function passwordEqual() {
+    if (rptpassword.value === input.value) {
+        check.innerHTML = `<p class='text-green-500 font-bold'>As senhas são iguais.</p>`
+    } else {
+        check.innerHTML = `<p class='text-red-500 font-bold'>As senhas são diferentes.</p>`
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/{index.html,cadastro.html,login.html,recuperar.html,editar_perfil.html}"],
+  content: [
+    "./src/{index.html,cadastro.html,login.html,recuperar.html,editar_perfil.html}",
+    "./src/js/script.js"
+  ],
   theme: {
     extend: {
       colors: {
